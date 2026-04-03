e33a4c9107fe250c66ddc2acdabff3a12d471506
CxRedix <realcxredix@gmail.com>
2026-04-03 17:20:45 +0800

    feat: rewrote the base logic of the code, and now all wands sprites are dynamically set within the code;

--- init.lua
+++ init.lua
@@ -3,7 +3,6 @@ dofile_once("data/scripts/lib/utilities.lua")
 
 local atw_common = dofile_once("mods/cxredix_animated_tower_wands/atw_common.lua")
 
-
 --- ### Adds a component to an entity file.
 --- ***
 --- @param file_path string The path to the file you wish to add a component to.
@@ -14,13 +13,74 @@ function ModEntityFileAddComponent(file_path, comp)
     ModTextFileSetContent(file_path, contents)
 end
 
-local wand_comp_append_paths = {
-    ["data/entities/items/wands/wand_good/wand_good_1.xml"] = atw_common.assets_path .. "swiftness/",
-    ["data/entities/items/wands/wand_good/wand_good_2.xml"] = atw_common.assets_path .. "destruction/",
-    ["data/entities/items/wands/wand_good/wand_good_3.xml"] = atw_common.assets_path .. "multitudes/",
+-- WAND INFO SETUP
+local WandInfo = {}
+WandInfo.__index = WandInfo
+
+function WandInfo.new(noita_sprite_path, noita_wand_entity_path, asset_path)
+    return setmetatable({
+        noita_sprite_path = noita_sprite_path,
+        noita_wand_entity_path = noita_wand_entity_path,
+        asset_path = asset_path,
+    }, WandInfo)
+end
+
+--- ### appends a string within the entity nxml tags.
+--- ***
+--- @param nxml_str string the raw nxml string to append within the wand entity.
+function WandInfo:append_raw_entity_components(nxml_str)
+    ModEntityFileAddComponent(
+        self.noita_wand_entity_path, nxml_str
+    )
+end
+
+function WandInfo:set_static_sprite(static_sprite_file_name_relative)
+    local static_sprite = ModTextFileGetContent(self.asset_path .. static_sprite_file_name_relative)
+    ModTextFileSetContent(self.noita_sprite_path, static_sprite)
+end
+
+-- tower wand id
+local TW_ID = {
+    TW_ID_BEGIN = 1,
+
+    swiftness = 1,
+    destruction = 2,
+    multitudes = 3,
+
+    TW_ID_END = 3,
 }
 
-local nxml_wand_animation_comp = ([[
+local noita_wand_sprite_base_path = "data/items_gfx/wands/custom/"
+local noita_wand_entity_base_path = "data/entities/items/wands/wand_good/"
+
+local swiftness = WandInfo.new(
+    noita_wand_sprite_base_path .. "good_01.xml",
+    noita_wand_entity_base_path .. "wand_good_1.xml",
+    (atw_common.assets_path .. "swiftness/")
+)
+
+local destruction = WandInfo.new(
+    noita_wand_sprite_base_path .. "good_02.xml",
+    noita_wand_entity_base_path .. "wand_good_2.xml",
+    (atw_common.assets_path .. "destruction/")
+)
+
+local multitudes = WandInfo.new(
+    noita_wand_sprite_base_path .. "good_03.xml",
+    noita_wand_entity_base_path .. "wand_good_3.xml",
+    (atw_common.assets_path .. "multitudes/")
+)
+
+local tower_wands = {
+    [TW_ID.swiftness]   = swiftness,
+    [TW_ID.destruction] = destruction,
+    [TW_ID.multitudes]  = multitudes
+}
+
+-- handle static sprite xml setup
+
+-- append lua components
+local wand_common_animation_comp = ([[
     <LuaComponent
         _enabled="1"
         _tags="enabled_in_world,enabled_in_hand"
@@ -29,22 +89,28 @@ local nxml_wand_animation_comp = ([[
     />
 ]]):format(atw_common.assets_path .. "atw_animated_replace.lua")
 
-for wand_xml_path, wand_assets_path in pairs(wand_comp_append_paths) do
-    ModEntityFileAddComponent(wand_xml_path, nxml_wand_animation_comp)
+for tw_id, tower_wand in pairs(tower_wands) do
+    -- setup static sprite
+    tower_wand:set_static_sprite("static_01.xml")
 
-    ModEntityFileAddComponent(wand_xml_path, ([[
+    -- append animation change
+    tower_wand:append_raw_entity_components(wand_common_animation_comp)
+
+    -- append individual script files for each tower wand
+    tower_wand:append_raw_entity_components(([[
     <LuaComponent
         _enabled="1"
         _tags="enabled_in_world,enabled_in_hand"
         execute_every_n_frame="30"
-        script_source_file="%s/script.lua"
+        script_source_file="%s"
     />
-    ]]):format(wand_assets_path))
+    ]]):format(tower_wand.asset_path .. "script.lua"))
 end
 
 
+-- MULTITUDES PARTICLE EMITTERS --
 ModEntityFileAddComponent(
-    "data/entities/items/wands/wand_good/wand_good_3.xml", [[
+    tower_wands[TW_ID.multitudes].noita_wand_entity_path, [[
     <ParticleEmitterComponent 
         _tags="enabled_in_world,enabled_in_hand,tower_wand_emitter"
 
